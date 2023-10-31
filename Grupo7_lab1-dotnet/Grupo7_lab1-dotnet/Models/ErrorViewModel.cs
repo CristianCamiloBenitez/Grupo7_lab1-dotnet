654263b8e8cd9a3840b4f0d2fd87fae555d2e53e
Cristian Camilo Benitez Peña <78176029+CristianCamiloBenitez@users.noreply.github.com>
2023-10-30 22:21:59 -0500

    Entities

---
+++ Grupo7_lab1-dotnet/Grupo7_lab1-dotnet/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Grupo7_lab1_dotnet.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
